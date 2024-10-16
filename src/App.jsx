90ba1ebdafb0ee2825534e2f65e8bc5eef120418
omobolaji <omobolaji119@gmail.com>
2024-10-16 23:35:34 +0100

    made minor changes

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,4 @@
+import './App.css'
 import HeroSection from './Components/HeroSection'
 import Support from "./Components/support"
 import Features from "./Components/Features"
@@ -7,14 +8,14 @@ import Footer from "./Components/Footer"
 
 function App() {
   return (
-    <div>
+    <>
       <HeroSection />
       <Support />
       <Features />
       <Benefits />
       <RightPlan />
       <Footer />
-    </div>
+    </>
   )
 }
 //figma link https://www.figma.com/design/aDOGDJ5kk7MA29e5TGqBwS/Saas-Landing-Pages-(Community)?t=MpqD83u1jdimi0U2-0
